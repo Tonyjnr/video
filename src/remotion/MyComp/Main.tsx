8d6e7edbda4ae5007de613c7bb95fec9b548d211
Otele Damilola David <damy84511@gmail.com>
2025-06-17 15:53:24 +0000

    Increase text size for visual emphasis and adjust audio sequence settings

--- src/remotion/MyComp/Main.tsx
+++ src/remotion/MyComp/Main.tsx
@@ -24,22 +24,33 @@ export const Main = ({ title }: z.infer<typeof CompositionProps>) => {
         endAt={2.04 * fps}
         playbackRate={0.8}
       />
+      {/* <Sequence from={2.0 * fps} durationInFrames={1.43 * fps} name="song"> */}
+
+      {/* </Sequence> */}
       <Sequence durationInFrames={1.03 * fps} name="intro">
         <div className="flex items-center justify-center h-full w-full">
-          <h1 className="text-[70px] font-bold">I</h1>
+          <h1 className="text-[200px] font-bold">I</h1>
         </div>
       </Sequence>
       <Sequence from={1.03 * fps} durationInFrames={0.5 * fps} name="hope">
         <div className="flex items-center justify-center h-full w-full">
-          <h1 className="text-[70px] font-bold">HOPE</h1>
+          <h1 className="text-[200px] font-bold">HOPE</h1>
         </div>
       </Sequence>
       <Sequence from={1.5 * fps} durationInFrames={0.53 * fps} name="you">
         <div className="flex items-center justify-center h-full w-full">
-          <h1 className="text-[70px] font-bold">YOU</h1>
+          <h1 className="text-[200px] font-bold">YOU</h1>
         </div>
       </Sequence>
       <Sequence from={2.01 * fps} durationInFrames={43} name="find">
+        <Audio
+          src={staticFile("song.mp3")}
+          startFrom={1.58 * fps}
+          from={2.04 * fps}
+          endAt={3.3 * fps}
+          playbackRate={0.7}
+          name="FIND"
+        />
         <div className="flex items-center justify-center h-full w-full relative">
           <Gif
             ref={ref}
@@ -50,8 +61,8 @@ export const Main = ({ title }: z.infer<typeof CompositionProps>) => {
             playbackRate={1}
           />
           <h1
-            style={{ webkitTextStroke: "6px white" }}
-            className="text-[200px] font-bold absolute text-transparent stroke-white stroke-2"
+            style={{ webkitTextStroke: "10px #eeeeee" }}
+            className="text-[500px] font-bold absolute text-transparent stroke-[#eeeeee] stroke-2"
           >
             FIND
           </h1>
